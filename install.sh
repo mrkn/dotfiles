2e66e6ae50f1eb4c841d609b46e9ce4ea950c6a7
Kenta Murata <mrkn@mrkn.jp>
2016-12-21 10:06:14 +0900

    Make install.sh runnable

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+export DESTDIR=${DESTDIR:-${HOME}}
+export DOTFILES_REPO=https://github.com/mrkn/dotfiles
+
+## Deploy
+
+mkdir -p ${DESTDIR}/.dotfiles
+cd ${DESTDIR}/.dotfiles
+curl -fsSL ${DOTFILES_REPO}/tarball/master | /usr/bin/tar xz -m --strip 1
+
+
+## Install mitamae
+
+bin/install_mitamae
+
+
+## Run mitamae
+
+PATH=$HOME/bin:$PATH
+mitamae local bootstrap.rb
