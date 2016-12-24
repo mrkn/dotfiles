0a107d1125b5e6725980b5dae6ff2e57f7c84bcc
Kenta Murata <mrkn@mrkn.jp>
2016-12-24 20:54:05 +0900

    Set force=true for dotfile resource

--- recipes/modules/dotfile/default.rb
+++ recipes/modules/dotfile/default.rb
@@ -1,8 +1,11 @@
 config_dir = File.expand_path("../../../../config", __FILE__)
 
 define :dotfile do
+  params[:force] = true unless params.has_key? :force
+
   link File.join(ENV['HOME'], params[:name]) do
     to File.join(config_dir, params[:name])
+    force params[:force]
     owner params[:owner] if params[:owner]
     group params[:group] if params[:group]
   end
