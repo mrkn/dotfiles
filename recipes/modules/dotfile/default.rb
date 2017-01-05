86ab70b3239c870d915cac43adb4a76803ec4a86
Kenta Murata <mrkn@mrkn.jp>
2017-01-05 12:26:05 +0900

    Stop making symlinks to directories by dotfile resource

--- recipes/modules/dotfile/default.rb
+++ recipes/modules/dotfile/default.rb
@@ -3,6 +3,11 @@ config_dir = File.expand_path("../../../../config", __FILE__)
 define :dotfile do
   params[:force] = true unless params.has_key? :force
 
+  directory File.join(ENV['HOME'], File.dirname(params[:name])) do
+    owner params[:owner] if params[:owner]
+    group params[:group] if params[:group]
+  end
+
   link File.join(ENV['HOME'], params[:name]) do
     to File.join(config_dir, params[:name])
     force params[:force]
