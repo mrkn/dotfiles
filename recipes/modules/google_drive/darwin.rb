d06a3028b7124802ea638eafcd4f3e6a615ba9f6
Kenta Murata <mrkn@mrkn.jp>
2017-01-30 16:18:15 +0900

    darwin: Install Google Drive

---
+++ recipes/modules/google_drive/darwin.rb
@@ -0,0 +1,5 @@
+install_app 'Google Drive' do
+  version '1.32'
+  url 'https://dl.google.com/drive/installgoogledrive.dmg'
+  sha256 'e13099fcfac6f8bd7a0ac236067dc3d76e0ba9e961ffc4c5ed303488b5e3785e'
+end
