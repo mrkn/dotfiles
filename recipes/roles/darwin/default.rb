bbe24934ab6a48c7031de99ed248b4751e64906b
Kenta Murata <mrkn@mrkn.jp>
2017-02-17 11:18:35 +0900

    darwin: Install terminal-notifier

--- recipes/roles/darwin/default.rb
+++ recipes/roles/darwin/default.rb
@@ -45,6 +45,7 @@ include_module 'sdl2'
 include_module 'envchain'
 include_module 'amazon-ecs-cli'
 include_module 'imagemagick'
+include_module 'terminal-notifier'
 
 include_module 'alfred'
 include_module 'bartender'
