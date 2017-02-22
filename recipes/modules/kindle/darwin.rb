6a12a0104a71ad9d637a392ee5c1877a2c29a002
Kenta Murata <mrkn@mrkn.jp>
2017-02-22 09:26:15 +0900

    darwin: Install Kindle For Mac

---
+++ recipes/modules/kindle/darwin.rb
@@ -0,0 +1,6 @@
+install_app 'Kindle' do
+  version '1.19.1'
+  url 'https://www.amazon.co.jp/kindlemacdownload/ref=klp_hz_mac'
+  sha256 '2105e350b1b31cf8ccc4e35468c5ae42c7b34f7c7bbfe3b29677d6c731862328'
+  archive_name 'KindleForMac.dmg'
+end
--- recipes/roles/darwin/default.rb
+++ recipes/roles/darwin/default.rb
@@ -61,6 +61,7 @@ include_module 'rescue_time'
 include_module 'spark'
 include_module 'docker'
 include_module 'marp'
+include_module 'kindle'
 
 include_module 'rbenv'
 include_module 'ruby'
