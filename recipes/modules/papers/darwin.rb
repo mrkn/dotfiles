91cef899d7c65355b486e676fc67817679e1ff1a
Kenta Murata <mrkn@mrkn.jp>
2017-03-13 09:35:09 +0900

    papers: Update version

---
+++ recipes/modules/papers/darwin.rb
@@ -0,0 +1,6 @@
+install_app 'Papers' do
+  version '3.4.10'
+  url 'http://papersapp.com/download/mac'
+  sha256 'cef4ecc8f0968b500db1fe1cd0d525a6912d8e3e9b5035d416fe77236ba74132'
+  archive_name 'papers.dmg'
+end
--- recipes/modules/papers/default.rb
+++
@@ -1,9 +0,0 @@
-case node[:platform]
-when 'darwin'
-  install_app 'Papers' do
-    version '3.4.7'
-    url 'http://papersapp.com/download/mac'
-    sha256 'a3772fad2fc4c5c273b597e136d7ba07e9f19c88b401d481f760697beb70ebe4'
-    archive_name 'papers.dmg'
-  end
-end
