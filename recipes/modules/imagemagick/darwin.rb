2b7d10d8006a6ac3ea35e61c189b0b538f97c403
Kenta Murata <mrkn@mrkn.jp>
2017-05-15 11:27:46 +0900

    darwin: imagemagick@6

--- recipes/modules/imagemagick/darwin.rb
+++ recipes/modules/imagemagick/darwin.rb
@@ -41,9 +41,37 @@ package 'imagemagick' do
     --with-webp
     --without-x11
     --with-zero-configuration
-    --without-freetype
-    --without-jpeg
-    --without-libpng
-    --without-libtiff
+    --with-freetype
+    --with-jpeg
+    --with-libpng
+    --with-libtiff
   ].join(' ')
+end
+
+package 'imagemagick@6' do
+  #options %w[
+  #  --with-fftw
+  #  --with-fontconfig
+  #  --with-ghostscript
+  #  --with-hdri
+  #  --with-liblqr
+  #  --with-librsvg
+  #  --with-libwmf
+  #  --with-little-cms
+  #  --with-little-cms2
+  #  --with-opencl
+  #  --with-openexr
+  #  --with-openjpeg
+  #  --with-pango
+  #  --with-perl
+  #  --with-quantum-depth-16
+  #  --with-quantum-depth-32
+  #  --with-quantum-depth-8
+  #  --with-webp
+  #  --with-zero-configuration
+  #  --without-freetype
+  #  --without-jpeg
+  #  --without-libpng
+  #  --without-libtiff
+  #].join(' ')
 end
