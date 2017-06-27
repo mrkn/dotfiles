40ba2db1f70dd565885cc5fdcde5388606e72dec
Kenta Murata <mrkn@mrkn.jp>
2017-06-27 11:52:54 +0900

    darwin: Do launchctl load for mysql and postgresql

--- recipes/modules/mysql/darwin.rb
+++ recipes/modules/mysql/darwin.rb
@@ -1,3 +1,7 @@
 include_module 'openssl'
 
 package 'mysql'
+
+execute "launchctl load -w  $(brew --prefix mysql)/homebrew.mxcl.mysql.plist)" do
+  not_if "launchctl list | grep -q homebrew.mxcl.mysql"
+end
--- recipes/modules/postgresql/default.rb
+++ recipes/modules/postgresql/default.rb
@@ -1,4 +1,8 @@
 case node[:platform]
 when 'darwin'
   package 'postgresql'
+
+  execute "launchctl load -w  $(brew --prefix postgresql)/homebrew.mxcl.postgresql.plist)" do
+    not_if "launchctl list | grep -q homebrew.mxcl.postgresql"
+  end
 end
