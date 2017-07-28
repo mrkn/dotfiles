dd5a29fe498e070ce18f721aff138a2520687a1d
Kenta Murata <mrkn@mrkn.jp>
2017-07-28 14:06:04 +0900

    Install python 3.6.1-o0

--- recipes/modules/python/3.6.rb
+++ recipes/modules/python/3.6.rb
@@ -3,4 +3,11 @@ install_python '3.6.1' do
   env PYTHON_CONFIGURE_OPTS: '--enable-framework'
 end
 
+install_python '3.6.1' do
+  prefix '3.6.1-o0'
+  keep true
+  env PYTHON_CONFIGURE_OPTS: '--enable-framework',
+      OPT: '-g -gdwarf-5 -O0 -Wall -Wstrict-prototypes'
+end
+
 execute 'pyenv global 3.6.1'
--- recipes/modules/python/helper.rb
+++ recipes/modules/python/helper.rb
@@ -1,8 +1,8 @@
-define :install_python, keep: false, force: nil, env: {} do
+define :install_python, keep: false, force: nil, debug: false, env: {}, prefix: nil do
   rebuild_flag_name = "REBUILD_PYTHON_#{params[:name].gsub('.', '_').upcase}"
   params[:force] ||= ENV[rebuild_flag_name]
 
-  local_ruby_block "Install python #{params[:name]}" do
+  local_ruby_block "Install python #{params[:prefix] || params[:name]}" do
     block do
       save_envs = {}
       params[:env].each do |key, val|
@@ -11,10 +11,19 @@ define :install_python, keep: false, force: nil, env: {} do
       end
 
       begin
-        cmdline = ['pyenv', 'install']
+        if params[:prefix]
+          cmdline = [File.expand_path('~/.pyenv/plugins/python-build/bin/python-build')]
+        else
+          cmdline = ['pyenv', 'install']
+          cmdline << (params[:force] ? '-f' : '-s')
+        end
         cmdline << '-k' if params[:keep]
-        cmdline << (params[:force] ? '-f' : '-s')
+        cmdline << '-g' if params[:debug]
         cmdline << params[:name]
+        if params[:prefix]
+          ENV['PYTHON_BUILD_BUILD_PATH'] = File.expand_path(params[:prefix], '~/.pyenv/sources')
+          cmdline << File.expand_path(params[:prefix], '~/.pyenv/versions')
+        end
         run_command(cmdline)
       ensure
         save_envs.each do |key, val|
