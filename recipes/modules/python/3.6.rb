856d35eda62579c65ea55a5b747b80aa31002ae9
Kenta Murata <mrkn@mrkn.jp>
2017-07-28 16:05:52 +0900

    Fix debug option for python 3.6.1-o0

--- recipes/modules/python/3.6.rb
+++ recipes/modules/python/3.6.rb
@@ -7,7 +7,7 @@ install_python '3.6.1' do
   prefix '3.6.1-o0'
   keep true
   env PYTHON_CONFIGURE_OPTS: '--enable-framework',
-      OPT: '-g -gdwarf-5 -O0 -Wall -Wstrict-prototypes'
+      OPT: '-g -O0 -Wall -Wstrict-prototypes'
 end
 
 execute 'pyenv global 3.6.1'
