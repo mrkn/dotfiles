7c660504899f688bc79588e4226673f07c169075
Kenta Murata <mrkn@mrkn.jp>
2017-07-28 11:58:23 +0900

    postgresql: Fix the commandline of launchctl

--- recipes/modules/postgresql/default.rb
+++ recipes/modules/postgresql/default.rb
@@ -2,7 +2,7 @@ case node[:platform]
 when 'darwin'
   package 'postgresql'
 
-  execute "launchctl load -w  $(brew --prefix postgresql)/homebrew.mxcl.postgresql.plist)" do
+  execute "launchctl load -w  $(brew --prefix postgresql)/homebrew.mxcl.postgresql.plist" do
     not_if "launchctl list | grep -q homebrew.mxcl.postgresql"
   end
 end
