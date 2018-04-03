f8f93cbb11474fc6278c7a6535b9d762205cfee4
Kenta Murata <mrkn@mrkn.jp>
2018-04-03 09:44:18 +0900

    Extract dotfile recipe

---
+++ recipes/roles/darwin/dotfile.rb
@@ -0,0 +1,24 @@
+include_module 'dotfile'
+
+dotfile '.bash_aliases'
+dotfile '.bash_profile'
+dotfile '.bash_profile.d/000_locale.sh'
+dotfile '.bash_profile.d/100_homebrew.sh'
+dotfile '.bash_profile.d/101_cache.sh'
+dotfile '.bash_profile.d/200_functions.sh'
+dotfile '.bash_profile.d/500_rbenv.sh'
+dotfile '.bash_profile.d/510_pyenv.sh'
+dotfile '.bash_profile.d/800_iterm2.sh'
+dotfile '.bash_profile.d/810_vscode.sh'
+dotfile '.bash_profile.d/900_home_bin.sh'
+dotfile '.bash_profile.d/910_aliases.sh'
+
+dotfile '.config/nvim/init.vim'
+
+dotfile '.mume/style.less'
+
+dotfile '.gemrc'
+dotfile '.gitconfig'
+dotfile '.tmux.conf'
+dotfile '.dein.toml'
+dotfile '.vimrc'
