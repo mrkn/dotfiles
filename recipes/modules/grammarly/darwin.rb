463a5e08abef191cb79736226dc547ffc275ed99
Kenta Murata <mrkn@mrkn.jp>
2018-05-07 17:53:30 +0900

    Add Grammarly in darwin

---
+++ recipes/modules/grammarly/darwin.rb
@@ -0,0 +1,7 @@
+grammarly_version = '1.5.36'
+
+install_app 'Grammarly' do
+  url 'http://download-editor.grammarly.com/osx/Grammarly.dmg'
+  version grammarly_version
+  sha256 '896909709b47fde6c9d5c2b360682ef2b22f1bb92d1e5a06f29327f40fd2c199'
+end
