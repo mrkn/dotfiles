90ae18897f51f2fc09131734de3d533546fbb3a1
Kenta Murata <mrkn@mrkn.jp>
2018-05-07 09:20:04 +0900

    Fix mysql for darwin

--- recipes/modules/mysql/darwin.rb
+++ recipes/modules/mysql/darwin.rb
@@ -2,6 +2,6 @@ include_module 'openssl'
 
 package 'mysql'
 
-execute "launchctl load -w  $(brew --prefix mysql)/homebrew.mxcl.mysql.plist)" do
+execute "launchctl load -w  $(brew --prefix mysql)/homebrew.mxcl.mysql.plist" do
   not_if "launchctl list | grep -q homebrew.mxcl.mysql"
 end
