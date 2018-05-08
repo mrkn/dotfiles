8218f035840940113928c58abbc0b2d4670d0251
Kenta Murata <mrkn@mrkn.jp>
2018-05-08 09:09:42 +0900

    Update Alfred in darwin

--- recipes/modules/alfred/default.rb
+++ recipes/modules/alfred/default.rb
@@ -1,8 +1,8 @@
-alfred_version = '3.2.1'
-bundle_version = 768
+alfred_version = '3.6.1'
+bundle_version = 910
 
 install_app 'Alfred 3' do
   version alfred_version
   url "https://cachefly.alfredapp.com/Alfred_#{alfred_version}_#{bundle_version}.dmg"
-  sha256 '88c7d139e043c15a1c4ee70c18ec8b482f4bc951d798f7026dc0e39802107841'
+  sha256 'fdefdb35047e193e1d06f5a441f4aabd4b45b24fd43c63d223d8508ad11a131e'
 end
