05b827165d3a656127e0e05c68ce2b8ae00daa03
Kenta Murata <mrkn@mrkn.jp>
2018-05-08 09:17:57 +0900

    Update duet in darwin

--- recipes/modules/duet/default.rb
+++ recipes/modules/duet/default.rb
@@ -1,9 +1,9 @@
-duet_version = '1.6.3.4'
+duet_version = '1.7.1.4'
 
 install_app 'duet' do
   version duet_version
   url 'http://www.duetdisplay.com/mac/'
   archive_name "duet-#{duet_version.gsub('.', '-')}.zip"
-  sha256 'e8beb6d3c1bee14410d6602d9d6a263a33a76a1c6c5476734070a745e375d96e'
-  open_after_install true
+  sha256 '77279033e3776af86eb7d54d1177a75edefed72d4c829eea53dc87bf92dd0004'
+  install_by :open
 end
