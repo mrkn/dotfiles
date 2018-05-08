355838a9bfe537988023352cf936f3c57128365e
Kenta Murata <mrkn@mrkn.jp>
2018-05-08 09:17:52 +0900

    Update Docker in darwin

--- recipes/modules/docker/darwin.rb
+++ recipes/modules/docker/darwin.rb
@@ -1,7 +1,7 @@
-docker_version = '1.13.0'
+docker_version = '18.03.1-ce-mac65'
 
 install_app 'Docker' do
   version docker_version
   url 'https://download.docker.com/mac/stable/Docker.dmg'
-  sha256 '1ff2a59d36348c2e1df73318e366624b04d42861e81af3d1b72effd803402641'
+  sha256 '678e2317427377bf6550af4cce7fcea10c4c860b9718fd9db4311350dd886fc2'
 end
