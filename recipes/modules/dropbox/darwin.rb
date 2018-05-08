d0e5cdb0a88fd6760417a424a5b4c31ab1d447ea
Kenta Murata <mrkn@mrkn.jp>
2018-05-08 09:10:04 +0900

    Update Dropbox in darwin

--- recipes/modules/dropbox/darwin.rb
+++ recipes/modules/dropbox/darwin.rb
@@ -1,9 +1,9 @@
-dropbox_version = '17.4.34'
+dropbox_version = '48.4.58'
 
 install_app 'Dropbox' do
   version dropbox_version
   url 'https://www.dropbox.com/download?plat=mac'
   archive_name 'DropboxInstaller.dmg'
-  sha256 '6bb0742f4cf43f640b3b84ce3e946ac5291eb71635f0f2c384fdeb47483395c1'
+  sha256 '4278bd0fc5f7f4fca47355f368a501f55261b55b0ac6d5b680cc62bfb60aa2ce'
   install_by :open
 end
