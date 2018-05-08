9b29399a1636c64ac1969a789a544d310840110f
Kenta Murata <mrkn@mrkn.jp>
2018-05-08 09:09:55 +0900

    Update Bartender in darwin

--- recipes/modules/bartender/default.rb
+++ recipes/modules/bartender/default.rb
@@ -3,5 +3,5 @@ bartender_version = '2.1.6'
 install_app 'Bartender 2' do
   version bartender_version
   url 'https://www.macbartender.com/Demo/Bartender%202.zip'
-  sha256 '013bb1f5dcc29ff1ecbc341da96b6e399dc3c85fc95bd8c7bee153ab0d8756f5'
+  sha256 '8a3779dc0ec30eabe2bca30eb35448ed86bd3b87bcfa5c3b9ec3dd9fe995e09a'
 end
