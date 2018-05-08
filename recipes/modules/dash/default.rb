faf2d89677a883c3aa7633124a74a8fe555fe196
Kenta Murata <mrkn@mrkn.jp>
2018-05-08 09:17:43 +0900

    Update Dash in darwin

--- recipes/modules/dash/default.rb
+++ recipes/modules/dash/default.rb
@@ -3,5 +3,5 @@ dash_version = '3.4.2'
 install_app 'Dash' do
   version dash_version
   url 'https://tokyo.kapeli.com/downloads/v3/Dash.zip'
-  sha256 '37b4552d7d5773054a914c512aa5db313c8a84c3da5e9756d0f374b695851bfc'
+  sha256 'a7fd1a759965d26a0de6a43b32aec857e3222fd02d4cfc6d0ef66a92da63d526'
 end
