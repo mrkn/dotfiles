a78f66c6b543802ebdabf15eb7755afe85318f22
Kenta Murata <mrkn@mrkn.jp>
2018-05-08 09:10:26 +0900

    Update RescueTime in darwin

--- recipes/modules/rescue_time/default.rb
+++ recipes/modules/rescue_time/default.rb
@@ -1,7 +1,7 @@
-rescue_time_version = ''
+rescue_time_version = '2.11.0.2538'
 
 install_app 'RescueTime' do
   version rescue_time_version
   url 'https://www.rescuetime.com/installers/RescueTimeInstaller.dmg'
-  sha256 '76c813e647d88805d596e38590d1ef551dc664920b5690a94709f0767d2b87fe'
+  sha256 '3cdfa6b7a54091b00c37881a78d0f77e05d62785b52ccb2e021b677b894d66e9'
 end
