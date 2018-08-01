4bb9ee5dc936a1fc9f972eb23bd2b9276727f235
Kenta Murata <mrkn@mrkn.jp>
2018-08-01 14:14:32 +0900

    ghq, peco: Support ubuntu platform

--- recipes/modules/ghq/default.rb
+++ recipes/modules/ghq/default.rb
@@ -1,4 +1,7 @@
 case node[:platform]
 when 'darwin'
   package 'ghq'
+when 'ubuntu'
+  include_module 'golang'
+  execute 'go get github.com/motemen/ghq'
 end
--- recipes/modules/peco/default.rb
+++ recipes/modules/peco/default.rb
@@ -1,4 +1,6 @@
 case node[:platform]
 when 'darwin'
   package 'peco'
+when 'ubuntu'
+  package 'peco'
 end
