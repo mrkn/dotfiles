a36b8e6e1d98ad8b88d23f3789c93aa852b443fb
Kenta Murata <mrkn@mrkn.jp>
2019-03-25 10:48:09 +0900

    Add previously forgotten files in julia recipe

---
+++ recipes/modules/julia/symlink.rb
@@ -0,0 +1,15 @@
+latest_ver = '1.1'
+
+case node[:platform]
+when 'darwin'
+  link File.expand_path("~/bin/julia") do
+    to "julia-#{latest_ver}"
+    force true
+  end
+
+  link File.expand_path("~/bin/julia-debug") do
+    to "julia-#{latest_ver}-debug"
+    force true
+  end
+end
+
