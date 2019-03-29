bf05efe3d84ef88073580732bebb247772a0f74b
Kenta Murata <mrkn@mrkn.jp>
2019-03-29 13:07:36 +0900

    Add neovim in ubuntu

--- recipes/modules/neovim/darwin.rb
+++ recipes/modules/neovim/darwin.rb
@@ -1,1 +1,2 @@
 package 'neovim/neovim/neovim'
+
---
+++ recipes/modules/neovim/ubuntu.rb
@@ -0,0 +1,11 @@
+package 'software-properties-common'
+
+execute 'add-apt-repository -y ppa:neovim-ppa/stable'
+execute 'apt-get update'
+
+package 'neovim'
+
+package 'python3-dev'
+package 'python3-pip'
+
+execute 'pip3 install neovim'
