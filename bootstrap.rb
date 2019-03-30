0c2f54f2e47c9a04fec16b9127f43f125475633c
Kenta Murata <mrkn@mrkn.jp>
2019-03-30 17:02:20 +0900

    Fix permission of dotfiles on ubuntu

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,1 +1,7 @@
 include_recipe 'lib/recipe_helper'
+
+case node[:platform]
+when 'ubuntu'
+  node[:user]  = 'mrkn'
+  node[:group] = 'mrkn'
+end
--- recipes/modules/dotfile/default.rb
+++ recipes/modules/dotfile/default.rb
@@ -3,15 +3,25 @@ config_dir = File.expand_path("../../../../config", __FILE__)
 define :dotfile do
   params[:force] = true unless params.has_key? :force
 
-  directory File.join(ENV['HOME'], File.dirname(params[:name])) do
+  case node[:platform]
+  when 'ubuntu'
+    params[:owner] ||= node[:user]
+    params[:group] ||= node[:group]
+  end
+
+  target_fullpath = File.join(ENV['HOME'], params[:name])
+
+  directory File.dirname(target_fullpath) do
     owner params[:owner] if params[:owner]
     group params[:group] if params[:group]
   end
 
-  link File.join(ENV['HOME'], params[:name]) do
+  link target_fullpath do
     to File.join(config_dir, params[:name])
     force params[:force]
-    owner params[:owner] if params[:owner]
-    group params[:group] if params[:group]
+  end
+
+  if params[:owner] && params[:group]
+    execute "chown #{params[:owner]}:#{params[:group]} #{target_fullpath}"
   end
 end
