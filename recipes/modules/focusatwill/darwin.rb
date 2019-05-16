99ab835a172916cd2171471015540ed358e92a38
Kenta Murata <mrkn@mrkn.jp>
2019-05-16 16:35:26 +0900

    Add application of Focus@will

---
+++ recipes/modules/focusatwill/darwin.rb
@@ -0,0 +1,7 @@
+focusatwill_version = "1.5.0"
+
+install_app "focusatwill" do
+  version focusatwill_version
+  url "https://faw-desktop.s3.amazonaws.com/focusatwill-#{focusatwill_version}.dmg"
+  sha256 "bc5bc776a77bf9921403536bc91d3fceb80991ccb420efe14905bb39c93e82b6"
+end
