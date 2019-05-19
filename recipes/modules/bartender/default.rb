e279a71a0cb1eec7fe04699168dc8d14ddb2988c
Kenta Murata <mrkn@mrkn.jp>
2019-05-19 18:53:03 +0900

    Update bartender version

--- recipes/modules/bartender/default.rb
+++ recipes/modules/bartender/default.rb
@@ -1,7 +1,7 @@
-bartender_version = '2.1.6'
+bartender_version = '3.0.64'
 
-install_app 'Bartender 2' do
+install_app 'Bartender 3' do
   version bartender_version
-  url 'https://www.macbartender.com/Demo/Bartender%202.zip'
-  sha256 '8a3779dc0ec30eabe2bca30eb35448ed86bd3b87bcfa5c3b9ec3dd9fe995e09a'
+  url 'https://www.macbartender.com/Demo/Bartender%203.zip'
+  sha256 '40a65c09f41ff56cd95b247c94ebdeda78748253b86204d0e4617b3f3fd4e261'
 end
