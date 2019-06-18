e1b1e19f5e2acf7b1e35239f4f4efeb319be4b3e
Kenta Murata <mrkn@mrkn.jp>
2019-06-18 17:42:02 +0900

    Update Alfred version

--- recipes/modules/alfred/default.rb
+++ recipes/modules/alfred/default.rb
@@ -1,8 +1,8 @@
-alfred_version = '3.8.1'
-bundle_version = 961
+alfred_version = '4.0.2'
+bundle_version = 1088
 
-install_app 'Alfred 3' do
+install_app 'Alfred 4' do
   version alfred_version
   url "https://cachefly.alfredapp.com/Alfred_#{alfred_version}_#{bundle_version}.dmg"
-  sha256 '9eb1c72910357775b6684f27b189533645f9d65fce57c7e4dad247042afb6621'
+  sha256 "7f83c73dac84a02308fd84acf3b98824a19fb506db2c97982d080e33f1bf9896"
 end
