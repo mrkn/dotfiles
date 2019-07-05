b5bb700df874a60b2345caf0d4d3121ac7c6463d
Kenta Murata <mrkn@mrkn.jp>
2019-07-05 11:11:42 +0900

    Update Alacritty

--- recipes/modules/alacritty/darwin.rb
+++ recipes/modules/alacritty/darwin.rb
@@ -1,7 +1,7 @@
-ver = "0.3.2"
+ver = "0.3.3"
 
 install_app "Alacritty" do
   version ver
   url "https://github.com/jwilm/alacritty/releases/download/#{ver}/Alacritty-v#{ver}.dmg"
-  sha256 "020474b1ec3aa8532fbda5019503c5c862bff7a09c9539656ba5ae74cb585081"
+  sha256 "0e53bee6e52356273db6aa9a51247c028e07cf61c2c9be98908594d49a67465c"
 end
--- recipes/modules/alacritty/ubuntu.rb
+++ recipes/modules/alacritty/ubuntu.rb
@@ -1,6 +1,6 @@
-ver = "0.3.2"
+ver = "0.3.3"
 url = "https://github.com/jwilm/alacritty/releases/download/v#{ver}/Alacritty-v#{ver}-ubuntu_18_04_amd64.deb"
-sha256 = "884dadc9f97792f6a8ef6a062b116f69e25943ab2cab79072cdcc795d6615205"
+sha256 = "913a19f0fc8a4fe22ea74ee25b583c1cecaadb2033c224307ed27b87ca9b5a83"
 
 local_ruby_block "Install alacritty" do
   block do
