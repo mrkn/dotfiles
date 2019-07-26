5e2600c6cc5f7e938857e8802f800858ecf17265
Kenta Murata <mrkn@mrkn.jp>
2019-07-26 18:41:18 +0900

    Make sure homebrew is available at first on darwin platform

--- recipes/roles/darwin/default.rb
+++ recipes/roles/darwin/default.rb
@@ -1,5 +1,11 @@
-include_recipe 'common'
-begin
-  include_recipe node[:hostname]
-rescue
+homebrew_available = system('/opt/brew/bin/brew commands >/dev/null 2>&1')
+if not homebrew_available
+  # make sure homebrew is available at first
+  include_module 'homebrew'
+else
+  include_recipe 'common'
+  begin
+    include_recipe node[:hostname]
+  rescue
+  end
 end
