4662a8fd0b7151f9fd513e82b51899044314dd81
Kenta Murata <mrkn@mrkn.jp>
2019-07-26 18:40:20 +0900

    zeromq: Remove options for the latest version

--- recipes/modules/zeromq/default.rb
+++ recipes/modules/zeromq/default.rb
@@ -1,10 +1,4 @@
 case node[:platform]
 when 'darwin'
-  include_module 'libpgm'
-  include_module 'libsodium'
-  include_module 'norm'
-
-  package 'zeromq' do
-    options '--HEAD --with-libpgm --with-libsodium --with-norm'
-  end
+  package 'zeromq'
 end
