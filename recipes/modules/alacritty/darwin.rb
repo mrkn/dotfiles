d0b76b7961649c5fd5b0b226020245ec25b7105b
Kenta Murata <mrkn@mrkn.jp>
2019-07-29 18:18:17 +0900

    Fix Alacritty URL

--- recipes/modules/alacritty/darwin.rb
+++ recipes/modules/alacritty/darwin.rb
@@ -2,6 +2,6 @@ ver = "0.3.3"
 
 install_app "Alacritty" do
   version ver
-  url "https://github.com/jwilm/alacritty/releases/download/#{ver}/Alacritty-v#{ver}.dmg"
+  url "https://github.com/jwilm/alacritty/releases/download/v#{ver}/Alacritty-v#{ver}.dmg"
   sha256 "0e53bee6e52356273db6aa9a51247c028e07cf61c2c9be98908594d49a67465c"
 end
