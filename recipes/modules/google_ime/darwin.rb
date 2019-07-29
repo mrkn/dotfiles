90265a77f700b4d8aeba931d841e7dde1c14467a
Kenta Murata <mrkn@mrkn.jp>
2019-07-29 18:32:33 +0900

    Update sha256 digest of google_ime

--- recipes/modules/google_ime/darwin.rb
+++ recipes/modules/google_ime/darwin.rb
@@ -3,7 +3,7 @@ google_ime_version = ''
 install_app 'GoogleJapaneseInput' do
   version google_ime_version
   url 'https://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg'
-  sha256 'b2e9b7202ed472c5be79fc67c71b98b3faffb25f0499437b7661895e504a60d2'
+  sha256 'db9547c1c629c2c9c0994a0fb3fa4f5f6bf89a009dfa2ce6d8e5c52824b9bf33'
   installer_name 'GoogleJapaneseInput.pkg'
   install_by :open
 end
