06606190cc13969e571e0ca7bfdfbea05a12612f
Kenta Murata <mrkn@mrkn.jp>
2019-08-19 09:16:43 +0900

    Make julia symlinks also on the platform other than darwin

--- recipes/modules/julia/symlink.rb
+++ recipes/modules/julia/symlink.rb
@@ -1,15 +1,11 @@
 latest_ver = '1.2'
 
-case node[:platform]
-when 'darwin'
-  link File.expand_path("~/bin/julia") do
-    to "julia-#{latest_ver}"
-    force true
-  end
-
-  link File.expand_path("~/bin/julia-debug") do
-    to "julia-#{latest_ver}-debug"
-    force true
-  end
+link File.expand_path("~/bin/julia") do
+  to "julia-#{latest_ver}"
+  force true
 end
 
+link File.expand_path("~/bin/julia-debug") do
+  to "julia-#{latest_ver}-debug"
+  force true
+end
