024ed46562fb7bafa2f1d99da9e741b7f230a672
Kenta Murata <mrkn@mrkn.jp>
2019-08-27 13:03:15 +0900

    Add synergy

---
+++ recipes/modules/synergy/darwin.rb
@@ -0,0 +1,8 @@
+ver = '1.10.2'
+rev = '8c010140'
+
+install_app 'Synergy' do
+  version ver
+  url "https://binaries.symless.com/synergy/v1-core-standard/v#{ver}-stable-8c010140/synergy_#{ver}-stable_b126-#{rev}_macos.dmg"
+  sha256 "e85ab316c7783c3da8caba94f31210d80bb8c68f740a9d14322c3a20c8d2adf0"
+end
