08ebae509bfd4b9015fb41d451ddf796cd7eeb6d
Kenta Murata <mrkn@mrkn.jp>
2019-12-30 13:57:34 +0900

    Update postico

--- recipes/modules/postico/darwin.rb
+++ recipes/modules/postico/darwin.rb
@@ -1,8 +1,4 @@
-postico_version = '1.3.5'
-
 install_app 'Postico' do
-  version postico_version
   url "https://eggerapps.at/postico/download/"
-  archive_name "postico-#{postico_version}.zip"
-  sha256 '638c4312d32f27aa6d92fd737fb55e23916b2b4326a24dc4ca6f9204ab501f1d'
+  archive_name "postico.zip"
 end
