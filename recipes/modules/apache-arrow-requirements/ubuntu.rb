a9b0d8f3e2bb69b014749d9172f54db4765136c1
Kenta Murata <mrkn@mrkn.jp>
2020-02-17 12:29:06 +0900

    Update apache-arrow related things

--- recipes/modules/apache-arrow-requirements/ubuntu.rb
+++ recipes/modules/apache-arrow-requirements/ubuntu.rb
@@ -1,31 +1,52 @@
+package 'wget'
+package 'software-properties-common'
+package 'gpg-agent'
+
+llvm_version = 7
+llvm_apt_url = "http://apt.llvm.org/bionic/"
+llvm_apt_arch = "llvm-toolchain-bionic-#{llvm_version}"
+execute "wget -q -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -"
+execute %Q[apt-add-repository -y --update "deb #{llvm_apt_url} #{llvm_apt_arch} main"]
+package "clang-#{llvm_version}"
+package "clang-format-#{llvm_version}"
+package "clang-tidy-#{llvm_version}"
+package "llvm-#{llvm_version}-dev"
+
 package 'autoconf'
-package 'binutils'
 package 'bison'
 package 'ca-certificates'
 package 'ccache'
-package 'clang-7'
 package 'cmake'
 package 'flex'
 package 'g++'
 package 'gcc'
 package 'gdb'
 package 'git'
-package 'gtk-doc-tools'
-package 'lcov'
-package 'libboost-all-dev'
+package 'libbenchmark-dev'
+package 'libboost-filesystem-dev'
+package 'libboost-regex-dev'
+package 'libboost-system-dev'
+package 'libbrotli-dev'
 package 'libbz2-dev'
-package 'libdouble-conversion-dev'
 package 'libgflags-dev'
-package 'libgirepository1.0-dev'
+package 'liblz4-dev'
 package 'libprotobuf-dev'
 package 'libprotoc-dev'
+package 'libre2-dev'
 package 'libsnappy-dev'
 package 'libssl-dev'
-package 'llvm-7-dev'
+package 'libzstd-dev'
 package 'ninja-build'
-package 'protobuf-compiler'
 package 'pkg-config'
+package 'protobuf-compiler'
+package 'rapidjson-dev'
+package 'thrift-compiler'
 package 'tzdata'
+
+package 'gtk-doc-tools'
+package 'lcov'
+package 'libgirepository1.0-dev'
+package 'libdouble-conversion-dev'
 package 'valgrind'
 
 include_recipe 'python'
