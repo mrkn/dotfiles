b250f47e66d0f2dad68d08afdfaff6dc5b9abb9c
Kenta Murata <mrkn@mrkn.jp>
2020-03-04 17:01:12 +0900

    Add llvm-9 and llvm-10 on Ubuntu

---
+++ recipes/modules/llvm/ubuntu.rb
@@ -0,0 +1,33 @@
+execute "wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -"
+
+file "/etc/apt/sources.list.d/llvm.list" do
+  owner "root"
+  group "root"
+  content <<APT_LINES
+deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main
+deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main
+
+deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main
+deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main
+APT_LINES
+end
+
+execute "apt-get update"
+
+package "llvm-10"
+package "clang-10"
+package "lldb-10"
+package "lld-10"
+package "libfuzzer-10-dev"
+package "libc++-10-dev"
+package "libc++abi-10-dev"
+package "libomp-10-dev"
+
+package "llvm-9"
+package "clang-9"
+package "lldb-9"
+package "lld-9"
+package "libfuzzer-9-dev"
+package "libc++-9-dev"
+package "libc++abi-9-dev"
+package "libomp-9-dev"
