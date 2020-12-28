a12989290c8d1d87654d4ccd4d964efeb0f1b38b
Kenta Murata <mrkn@mrkn.jp>
2020-12-28 14:21:05 +0900

    Add Ruby 3.0

--- recipes/modules/ruby/default.rb
+++ recipes/modules/ruby/default.rb
@@ -1,7 +1,8 @@
 include_recipe 'prerequisites'
 
+include_recipe '3.0'
 include_recipe '2.7'
 include_recipe '2.6'
-include_recipe 'master'
 include_recipe '2.5'
 include_recipe '2.4'
+include_recipe 'master'
