7a7c61d3db1ed675faef1229058b3906b294e5b0
Kenta Murata <mrkn@mrkn.jp>
2021-11-12 21:20:54 +0900

    Use mas for installing Kindle in macOS

--- recipes/modules/kindle/darwin.rb
+++ recipes/modules/kindle/darwin.rb
@@ -1,6 +1,1 @@
-install_app 'Kindle' do
-  version '1.19.1'
-  url 'https://www.amazon.co.jp/kindlemacdownload/ref=klp_hz_mac'
-  sha256 "1b70345bb96394f5662c1dfedf15a318e83f259553c83d81e32fc1e25fa251ab"
-  archive_name 'KindleForMac.dmg'
-end
+mas_package 405399194, "Kindle"
