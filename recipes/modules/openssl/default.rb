3bf62b55aef413189254b858835a0ef280ae3301
Kenta Murata <mrkn@mrkn.jp>
2022-03-17 22:41:13 +0900

    Add openssl@1.0 for Ruby 2.3

--- recipes/modules/openssl/default.rb
+++ recipes/modules/openssl/default.rb
@@ -1,5 +1,8 @@
 case node[:platform]
 when 'darwin'
+  openssl_formula = File.expand_path("../openssl@1.0.rb", __FILE__)
+  package openssl_formula
+  package 'openssl@1.1'
   package 'openssl'
 when 'ubuntu'
   # TODO
