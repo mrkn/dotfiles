e2b4e389bee7c4883826d7b131e21e8027864694
Kenta Murata <mrkn@mrkn.jp>
2022-08-20 15:06:50 +0900

    Update Julia 1.6

--- recipes/modules/julia/1.6.rb
+++ recipes/modules/julia/1.6.rb
@@ -1,13 +1,38 @@
-julia_version = '1.6.2'
+julia_version = '1.6.7'
 ver = julia_version.split('.')[0,2].join('.')
 
 
-case node[:platform]
-when 'darwin'
+base_uri = "https://julialang-s3.julialang.org/bin"
+variations = {
+  "darwin" => {
+    "x86_64" => {
+      path: "mac/x64/#{ver}/julia-#{julia_version}-mac64.dmg",
+      sha256: "e7b3ad41ec6dd1dd7e6d811ff71682680e056e0f82fc6b17710546630092915a"
+    }
+  },
+
+  "ubuntu" => {
+    "x86_64" => {
+      path: "linux/x64/#{ver}/julia-#{julia_version}-linux-x86_64.tar.gz",
+      sha256: "6c4522d595e4cbcd00157ac458a72f8aec01757053d2073f99daa39e442b2a36"
+    }
+  }
+}
+
+# No aarch64 version is supplied for v1.6 series
+variations["darwin"]["arm64"] = variations["darwin"]["x86_64"]
+
+
+os = node[:platform]
+hi = node.instance_variable_get(:@backend).host_inventory
+arch = hi.backend.os_info[:arch]  # TODO: node[:kernel][:machine]
+
+case os
+when "darwin"
   install_app "Julia-#{ver}" do
     version julia_version
-    url "https://julialang-s3.julialang.org/bin/mac/x64/#{ver}/julia-#{julia_version}-mac64.dmg"
-    sha256 "6fb5e110a1dc7c715b3e824d2c9226b9cda3947cc7df15900c1bc14d4fc437c9"
+    url File.join(base_uri, variations[os][arch][:path])
+    sha256 variations[os][arch][:sha256]
   end
 
   link File.expand_path("~/bin/julia-#{ver}") do
@@ -21,10 +46,10 @@ when 'darwin'
   end
 
 
-when 'ubuntu'
-  url = "https://julialang-s3.julialang.org/bin/linux/x64/#{ver}/julia-#{julia_version}-linux-x86_64.tar.gz"
+when "ubuntu"
+  url = File.join(base_uri, variations[os][arch][:path])
   archive_name = File.basename(url)
-  sha256 = "3eb4b5775b0df1ad38f6c409e989501ab445c95bcb01ab02bd60f5bd1e823240"
+  sha256 = variations[os][arch][:sha256]
 
   local_ruby_block "Download and Install Julia #{julia_version}" do
     block do
