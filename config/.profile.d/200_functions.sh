c18d0aa2d5d9e82907dd777ce03a3119056083a0
Kenta Murata <mrkn@mrkn.jp>
2022-08-27 04:10:42 +0900

    Fix 200_functions.sh for zsh

--- config/.profile.d/200_functions.sh
+++ config/.profile.d/200_functions.sh
@@ -1,7 +1,7 @@
-function ghq_list_path_peco() {
+ghq_list_path_peco() {
   ghq list -p "$1" | peco
 }
 
-function cd_ghq_list_path_peco() {
+cd_ghq_list_path_peco() {
   cd "$(ghq_list_path_peco "$@")"
 }
