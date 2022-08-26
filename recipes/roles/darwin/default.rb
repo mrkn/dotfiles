0c7f05fef502aab1fd64483c848f8e527fa5b689
Kenta Murata <mrkn@mrkn.jp>
2022-08-27 04:45:24 +0900

    Stop install homebrew in darwin/default role

--- recipes/roles/darwin/default.rb
+++ recipes/roles/darwin/default.rb
@@ -1,11 +1,6 @@
-homebrew_available = system('/opt/brew/bin/brew commands >/dev/null 2>&1')
-if not homebrew_available
-  # make sure homebrew is available at first
-  include_module 'homebrew'
-else
-  include_recipe 'common'
-  begin
-    include_recipe node[:hostname]
-  rescue
-  end
+include_recipe 'common'
+
+begin
+  include_recipe node[:hostname]
+rescue
 end
