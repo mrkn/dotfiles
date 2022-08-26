958decf8d792cc60d8bf2886c9d7be9b360e4d75
Kenta Murata <mrkn@mrkn.jp>
2022-08-27 00:19:17 +0900

    Look HOMEBREW_PREFIX in homebrew recipe

--- recipes/modules/homebrew/darwin.rb
+++ recipes/modules/homebrew/darwin.rb
@@ -1,5 +1,7 @@
-unless ENV["PATH"].include?("/opt/homebrew/bin")
-  ENV['PATH'] = "#{brew_prefix}/bin:#{ENV['PATH']}"
+homebrew_prefix = ENV.fetch("HOMEBREW_PREFIX", "/opt/homebrew")
+
+unless ENV["PATH"].include?("#{homebrew_prefix}/bin")
+  ENV['PATH'] = "#{homebrew_prefix}/bin:#{ENV['PATH']}"
 end
 
 execute "brew update"
