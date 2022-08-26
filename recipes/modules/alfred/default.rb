7afc8e0b8cece9e8cd0e647954ad9cadcd160b21
Kenta Murata <mrkn@mrkn.jp>
2022-08-27 04:28:52 +0900

    Alfred 5

--- recipes/modules/alfred/default.rb
+++ recipes/modules/alfred/default.rb
@@ -1,7 +1,7 @@
-alfred_version = '4.6'
-bundle_version = 1266
+alfred_version = '5.0.1'
+bundle_version = 2067
 
-install_app 'Alfred 4' do
+install_app 'Alfred 5' do
   version alfred_version
   url "https://cachefly.alfredapp.com/Alfred_#{alfred_version}_#{bundle_version}.dmg"
   sha256 "1fbfc0b188f68751fe1b96b060afbbaddf06dda80ed9b5d1dd563f9904211708"
