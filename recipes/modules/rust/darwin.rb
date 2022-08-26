e38b7f84ad93321ece2416a546e9924616757e33
Kenta Murata <mrkn@mrkn.jp>
2022-08-27 04:44:17 +0900

    Update rust recipe

--- recipes/modules/rust/darwin.rb
+++ recipes/modules/rust/darwin.rb
@@ -2,5 +2,7 @@ execute "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
   not_if "test -x $HOME/.cargo/bin/cargo"
 end
 
+ENV["PATH"] = "#{ENV["HOME"]}/.cargo/bin:#{ENV["PATH"]}"
+
 execute "rustup override set stable"
 execute "rustup update stable"
