1bba968fbb0523cd8ef72393b0c51a4d015f5226
Kenta Murata <mrkn@mrkn.jp>
2022-08-28 14:30:48 +0900

    Switch goenv repository

--- recipes/modules/goenv/default.rb
+++ recipes/modules/goenv/default.rb
@@ -2,7 +2,7 @@ goenv_dir = File.expand_path("~/.goenv")
 plugins_dir = File.join(goenv_dir, 'plugins')
 
 git goenv_dir do
-  repository 'https://github.com/dataich/goenv.git'
+  repository 'https://github.com/syndbg/goenv.git'
 end
 
 ENV['PATH'] = "#{goenv_dir}/bin:#{ENV['PATH']}"
