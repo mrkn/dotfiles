835eb79ca0fe446fb479a40216c422970d234ed6
Kenta Murata <mrkn@mrkn.jp>
2022-12-15 17:29:48 +0900

    Fix bootstrap script for darwin

--- recipes/roles/darwin/bootstrap.rb
+++ recipes/roles/darwin/bootstrap.rb
@@ -2,11 +2,18 @@
 
 ## Install homebrew
 
-homebrew_install_script = "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
-execute %Q[/bin/bash -c "$(curl -fsSL ${homebrew_install_script)"] do
-  not_if "test -x /opt/homebrew/bin/brew"
+if File.directory?("/opt/homebrew/bin")
+  brew_prefix = "/opt/homebrew"
+else
+  brew_prefix = "/opt/brew"
 end
 
-unless ENV["PATH"].include?("/opt/homebrew/bin")
-  ENV['PATH'] = "#{brew_prefix}/bin:#{ENV['PATH']}"
+homebrew_install_script = "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+execute %Q[/bin/bash -c "$(curl -fsSL ${homebrew_install_script)"] do
+  not_if "test -x #{brew_prefix}"
+end
+
+brew_bin_dir = File.join(brew_prefix, "bin")
+unless ENV["PATH"].include?(brew_bin_dir)
+  ENV["PATH"] = "#{brew_bin_dir}:#{ENV["PATH"]}"
 end
