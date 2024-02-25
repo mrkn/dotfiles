1ae2557439665fb3e654a4b3ac5102f8f73d55d7
Kenta Murata <mrkn@mrkn.jp>
2024-02-25 19:45:07 +0900

    Make Ruby 3.3 default

--- recipes/modules/ruby/default.rb
+++ recipes/modules/ruby/default.rb
@@ -1,8 +1,9 @@
 include_recipe 'prerequisites'
 
-include_recipe '3.2'
-execute 'rbenv global 3.2'
+include_recipe '3.3'
+execute 'rbenv global 3.3'
 
+include_recipe '3.2'
 include_recipe '3.1'
 include_recipe '3.0'
 include_recipe '2.7'
