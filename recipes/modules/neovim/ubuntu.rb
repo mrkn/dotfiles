9e9c4e8b59e7191a77c88a129591a1801ab1ee1f
Kenta Murata <mrkn@mrkn.jp>
2024-06-06 15:38:20 +0900

    Make nvim symlink in home/bin

--- recipes/modules/neovim/ubuntu.rb
+++ recipes/modules/neovim/ubuntu.rb
@@ -8,6 +8,7 @@ end
 
 execute "curl -sfSL -o /opt/nvim/nvim.appimage https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
 execute "chmod a+x /opt/nvim/nvim.appimage"
+execute "ln -sf /opt/nvim/nvim.appimage #{ENV["HOME"]}/bin/nvim"
 
 package 'python3-dev'
 package 'python3-pip'
